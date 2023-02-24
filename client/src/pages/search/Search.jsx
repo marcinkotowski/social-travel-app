718bd57bb11811dd0a5e752e8d2f1211ed00cee6
marcinkotowski <marcin.kotowski02@gmail.com>
2023-02-24 21:05:08 +0100

    Add search params menu design

--- client/src/pages/search/Search.jsx
+++ client/src/pages/search/Search.jsx
@@ -3,20 +3,54 @@ import "./search.scss";
 import { IoMdSearch } from "react-icons/io";
 import { MdCancel } from "react-icons/md";
 import debounce from "lodash/debounce";
+import { useSearchParams } from "react-router-dom";
+import Posts from "../../components/posts/Posts";
+import { AiFillFire, AiOutlineClockCircle } from "react-icons/ai";
+import { MdFavorite } from "react-icons/md";
 
 const Search = () => {
-  const [query, setQuery] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams({});
+  const [selectedType, setSelectedType] = useState(
+    searchParams.get("type") || "location"
+  );
+  const [selectedCategory, setSelectedCategory] = useState(
+    searchParams.get("category") || "trnd"
+  );
+  const [query, setQuery] = useState(searchParams.get("q") || "");
+
+  const type = ["location", "profile"];
+  const category = [
+    {
+      name: "trending",
+      svg: <AiFillFire style={{ color: "#f7a100" }} />,
+      abbreviation: "trnd",
+    },
+    { name: "most popular", svg: <MdFavorite />, abbreviation: "mp" },
+    { name: "latest", svg: <AiOutlineClockCircle />, abbreviation: "lst" },
+  ];
 
   const handleFilter = useCallback(
     debounce((value) => {
-      /* Fetch */
+      if (value.length > 0) {
+        console.log("fetch: " + value);
+        /* Fetch */
+      }
     }, 1500),
     []
   );
 
   const handleChange = (value) => {
     setQuery(value);
-    // handleFilter(value);
+    handleFilter(value);
+    if (value.length > 0) {
+      setSearchParams({
+        q: value,
+        type: selectedType,
+        category: selectedCategory,
+      });
+    } else {
+      setSearchParams({});
+    }
   };
 
   return (
@@ -35,15 +69,42 @@ const Search = () => {
               onChange={(e) => handleChange(e.target.value)}
               maxLength="50"
             />
-
             {query && (
               <div className="icon cancel">
-                <MdCancel onClick={() => setQuery("")} />
+                <MdCancel onClick={() => handleChange("")} />
               </div>
             )}
           </label>
+          <div className="type params">
+            {type.map((name) => (
+              <button
+                onClick={() => {
+                  setSelectedType(name);
+                }}
+                className={name === selectedType ? "active" : ""}
+              >
+                {name}
+              </button>
+            ))}
+          </div>
+          {selectedType != "profile" && !query && (
+            <div className="category params">
+              {category.map(({ name, svg, abbreviation }) => (
+                <button
+                  onClick={() => {
+                    setSelectedCategory(abbreviation);
+                  }}
+                  className={
+                    abbreviation === selectedCategory && !query ? "active" : ""
+                  }
+                >
+                  {svg}
+                  {name}
+                </button>
+              ))}
+            </div>
+          )}
         </div>
-        <div className="results"></div>
       </div>
     </div>
   );
